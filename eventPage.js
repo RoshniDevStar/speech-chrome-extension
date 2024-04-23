3e617dbce4faf731468b9308bda5bb0c8ab4393a
Roshni <144480629+RoshniDevStar@users.noreply.github.com>
2024-04-23 17:13:37 +0530

    Add files via upload

---
+++ eventPage.js
@@ -0,0 +1,11 @@
+chrome.contextMenus.create({
+  id: "speak",
+  title: "Speak",
+  contexts: ["selection"]
+});
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+  if (clickData.menuItemId === "speak" && clickData.selectionText) {
+    chrome.tts.speak(clickData.selectionText, { rate: 0.7 });
+  }
+});
